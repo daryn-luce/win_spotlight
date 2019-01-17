1ce94db55fbb66f8d3c83ccc9f12bc2fca15ad5d
Daryn Luce <daryn.luce@gmail.com>
2019-01-17 14:17:49 -0600

    added minior changes and comments to code

--- spotlight.py
+++ spotlight.py
@@ -3,16 +3,26 @@ import filecmp
 import pygame
 from shutil import copyfile
 
-pic_dir = "C:\\Users\\" + os.getlogin() + "\\Pictures\\wpp\\"
+# The two directories we're working with pictures and spotlight
+pic_dir = "C:\\Users\\" + os.getlogin() + "\\Pictures\\wp\\"
 spotlight_dir = "C:\\Users\\" + os.getlogin() + "\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets\\"
 
+# gets copies the files from the spotlight dir to the picture dir
+# as long as it's greater than 100kb and has a width of 1920px
 def getFiles(spot_dir, save_dir):
 	for files in os.listdir(spot_dir):
 		if (os.path.getsize(spot_dir + files) > 100000):
-			copyfile(spot_dir + files,save_dir + files + ".png")
-			tempimg = pygame.image.load(save_dir + files + ".png")
+			copyfile(spot_dir + files,save_dir + files + ".jpg")
+			tempimg = pygame.image.load(save_dir + files + ".jpg")
 			if(tempimg.get_width() != 1920):
-				os.remove(save_dir + files + ".png")
+				os.remove(save_dir + files + ".jpg")
+
+# Removes duplicates by looping through the directory twice and
+# ignoreing files with the same name then collects duplicates
+# Then goes through the duplicate list and removes any wallpapers
+# we don't already have.
+
+# need to come back to this method. (Error if there are more than 2 identical files) 
 
 def removeDuplicates(dir):
 	del_files = []
@@ -20,12 +30,14 @@ def removeDuplicates(dir):
 		for comp_files in os.listdir(dir):
 			if(files != comp_files):
 				if(filecmp.cmp(dir + files, dir + comp_files)):
-					#print("Removing file" + files + " becuase it's a duplicate")
-					del_files.append(files)
+					del_files.append(files)					
 	for file in del_files:
 		if(file[:9] != "Spotlight"):
 			os.remove(dir + file)
 			
+# renames the contents of the directoy for readability and so 
+# we can check new files with ones we already have. through our 
+# naming scheme
 		
 def renameWallPaper(dir):
 	i = 0
@@ -33,14 +45,12 @@ def renameWallPaper(dir):
 		i += 1
 		os.rename(dir + files, dir + "Spotlight_wp" + str(i))
 	for files in os.listdir(dir):
-		os.rename(dir + files, dir + files + ".png")
+		os.rename(dir + files, dir + files + ".jpg")
 
-#print(filecmp.cmp(pic_dir + "\\a.jpg", pic_dir + "\\c.jpg"))
-#print(pygame.image.load(pic_dir + "\\a.jpg").get_width())
-# copyfile(pic_dir + "\\a", pic_dir + "\\new\\a.jpg")
+# checks if directory exsist(creates it otherwise), then runs our functions
+# first getting the images, then removing duplicates, finally renaming.
 	
 if(os.path.isdir(pic_dir)):
-	#curLen = (len(os.listdir(pic_dir)) + 1)
 	getFiles(spotlight_dir, pic_dir)
 	removeDuplicates(pic_dir)
 	renameWallPaper(pic_dir)
@@ -48,4 +58,6 @@ if(os.path.isdir(pic_dir)):
 else:
 	os.mkdir(pic_dir)
 	getFiles(spotlight_dir, pic_dir)
+	removeDuplicates(pic_dir)
+	renameWallPaper(pic_dir)
 	
